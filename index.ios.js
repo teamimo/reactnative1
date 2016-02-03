fbc58e77e02374e41dbfb0deede001acaf8425c4
Ani Kunaparaju <ani@anivk.com>
2016-02-02 22:44:27 -0600

    Test commit
    Testing a quick change in master

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,7 @@ class reactnative1 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to In Memory Of!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
